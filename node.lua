14ed484224ebf13add656255691a52631667447a
Joe Elia <44442621+joeelia@users.noreply.github.com>
2019-09-03 14:33:31 -0400

    Update node.lua

--- node.lua
+++ node.lua
@@ -2,6 +2,14 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local json = require "json"
 
+local save_proof_of_play{
+     asset_id = asset_id, -- What are we playing
+     duration = duration, -- .. for how long
+     start = os.time(), -- unix timestamp of when playback started
+     device = sys.getenv "SERIAL", -- the serial number of the device
+     uuid = generate_some_uuid(), -- see below
+}
+
 local shaders = {
     multisample = resource.create_shader[[
         uniform sampler2D Texture;
